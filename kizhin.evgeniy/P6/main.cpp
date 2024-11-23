45e22918c0692a223ca9df84adeecd3f8b209bcf
fox <e.kizhin@yandex.ru>
2024-11-23 08:08:29 +0300

    Add main.cpp implementation
    Added main function that reads string from stream using
    string_utils module and then outputs whether a string complies
    with the grammar rules specified by the task

--- kizhin.evgeniy/P6/main.cpp
+++ kizhin.evgeniy/P6/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string_utils.hpp>
+#include "parser.hpp"
 
+int main()
+{
+  char* inputString = kizhin::readString(std::cin);
+  if (!inputString) {
+    std::cerr << "Failed to input string\n";
+    return 1;
+  }
+  std::cout << std::boolalpha << kizhin::parse(inputString) << '\n';
+  free(inputString);
+}
