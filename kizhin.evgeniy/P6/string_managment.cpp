a6777ec391b03e34f1bcdc5e886e679693974d1a
fox <e.kizhin@yandex.ru>
2024-11-23 07:52:57 +0300

    Add string_managment implementation
    This commit introduces a new module for managing a shared string
    between different modules. The implementation includes:
    - setString: Initializes the string
    - position: Returns pointer to the current position in the string
    - incrementPosition: Moves the position pointer forward
    - releaseString: Resetes string state to nullptr

--- kizhin.evgeniy/P6/string_managment.cpp
+++ kizhin.evgeniy/P6/string_managment.cpp
@@ -1,1 +1,36 @@
+#include "string_managment.hpp"
+#include <stdexcept>
+
+static const char* parsingString = nullptr;
+static const char* position_ = nullptr;
+
+void kizhin::detail::setString(const char* string)
+{
+  if (parsingString == nullptr) {
+    parsingString = string;
+    position_ = string;
+  } else {
+    throw std::logic_error("String must be released before reseting.");
+  }
+}
+
+const char* kizhin::detail::position()
+{
+  return position_;
+}
+
+void kizhin::detail::incrementPosition()
+{
+  if (parsingString != nullptr) {
+    ++position_;
+  } else {
+    throw std::logic_error("Can't increment to empty position.");
+  }
+}
+
+void kizhin::detail::releaseString()
+{
+  parsingString = nullptr;
+  position_ = nullptr;
+}
 
--- kizhin.evgeniy/P6/string_managment.hpp
+++ kizhin.evgeniy/P6/string_managment.hpp
@@ -1,1 +1,13 @@
+#ifndef STRING_MANAGEMENT_HPP
+#define STRING_MANAGEMENT_HPP
 
+namespace kizhin {
+  namespace detail {
+    void setString(const char*);
+    const char* position();
+    void incrementPosition();
+    void releaseString();
+  }
+}
+
+#endif
